f80f16a5896643a4bee9b5d9671e482843d2537e
Qasim Ali <qasimali24@gmail.com>
2026-01-07 15:09:22 +0000

    event detail loaded

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const indexController = require('../controllers/indexController');
 
 router.get('/', indexController.index);
+router.get('/event-detail', (req,res) => {
+    res.render('event-detail');
+});
 
 module.exports = router;
